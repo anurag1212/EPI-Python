497dd2b69417dce3789b5f6dcaf802b664182431
Anurag Pande <anurag1212@ucla.edu>
2018-10-23 19:26:19 -0700

    Finished greedy algos

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -2,8 +2,30 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(heights):
-    # TODO - you fill in here.
-    return 0
+
+    def find_pillars(h):
+        stk, pillars = [], []
+        for i in range(len(h)):
+            if not stk:
+                pillars.append(-1)
+            else:
+                while stk and stk[-1][0] >= h[i]:
+                    stk.pop()
+                if not stk:
+                    pillars.append(-1)
+                else:
+                    pillars.append(stk[-1][1])
+            stk.append((h[i], i))
+        return pillars
+
+    l = find_pillars(heights)
+    r = [len(heights) - i - 1 for i in list(reversed(find_pillars(list(reversed(heights)))))]
+
+    area = 0
+    for i in range(len(heights)):
+        area = max(area, heights[i] * (r[i]-(l[i]+1)))
+
+    return area
 
 
 if __name__ == '__main__':
